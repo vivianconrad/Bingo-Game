51a8d901c4fb362b64211779f74e7b81918c24bd
vivian.conrad <vivian.conrad@MU-C1-21.mullen.pvt.k12.co.us>
2018-10-17 10:22:22 -0600

    finish diagonal bingo start blackout game

--- Bingo Game/src/Bingo.java
+++ Bingo Game/src/Bingo.java
@@ -3,46 +3,59 @@ import java.util.Scanner;
 
 public class Bingo
 	{
-		static Scanner userInput = new Scanner(System.in);
+		static Scanner userInputStrings = new Scanner(System.in);
+		static Scanner userInputInt = new Scanner(System.in);
 		static String name;
 		static int gameChoice;
 		public static void main(String[] args)
 			{
 				//userWelcome();
-				getEazyBingoCards();
-				playGame();
+				decideGameMode();
+				playBingoGame();
+				blackoutGame();
 			}
 		public static void userWelcome()
 			{
 				System.out.println("Hello! What's your name?");
-				name = userInput.nextLine();
+				name = userInputStrings.nextLine();
 				System.out.println("Hi " + name + "! Welcome to BINGO! Press enter for the game rules.");
-				String resume = userInput.nextLine();
+				String resume = userInputStrings.nextLine();
 				System.out.println("The caller will say a letter and a number.");
 				System.out.println("If you have the number, then you will recieve an X.");
 				System.out.println("If you get 5 X's in a row, column or diagonal, then you win.");
 				System.out.println("BUT if the computer gets 5 X's first, then you lose.");
-				System.out.println("Also, the computer's name is Jeff the Wonder Dog");
+				System.out.println("Also, the computer's name is Jeff the Wonder Dog");	
+				System.out.println("Press enter to continue");
+				String resume2 = userInputStrings.nextLine();
+			}
+		public static void decideGameMode()
+			{
 				System.out.println("Would you like eazy, medium or hard? Press 1 for easy, 2 for medium, 3 for hard");
-				gameChoice = userInput.nextInt();
+				gameChoice = userInputInt.nextInt();
 				if (gameChoice == 1)
 					{
 						getEazyBingoCards();
 					}
-				if (gameChoice == 2)
+				else if (gameChoice == 2)
 					{
 						getMediumBingoCards();
 					}
-				if (gameChoice == 3)
+				else if (gameChoice == 3)
 					{
 						getHardBingoCards();
+					}
+				else 
+					{
+						System.out.println("You suck");
+						System.out.println("No BINGO for you");
+						System.exit(0);
 					}
 			}
 		public static void getEazyBingoCards()
 			{
 				gameChoice = 1;
 				System.out.println(name + ", press enter to get your bingo card");
-//				String getCard = userInput.nextLine();
+				String getCard = userInputStrings.nextLine();
 				UserCard.createCard();
 				UserCard.userBoard= new String [5][5];
 				for (int i=0; i<5; i++)
@@ -56,7 +69,7 @@ public class Bingo
 					}
 				UserCard.display();
 				System.out.println(name + ", press enter to get Jeff's card");
-//				String jeffCard = userInput.nextLine();
+				String jeffCard = userInputStrings.nextLine();
 				ComputerCard.createCard();
 				ComputerCard.jeffBoard= new String [5][5];
 				for (int i=0; i<5; i++)
@@ -69,11 +82,14 @@ public class Bingo
 							}
 					}
 				ComputerCard.display();
+				System.out.println("Press enter for your first calling number!");
+				String firstNum= userInputStrings.nextLine();
 			}
 		public static void getMediumBingoCards()
 			{
+				gameChoice = 2;
 				System.out.println(name + ", press enter to get your bingo card");
-//				String getCard = userInput.nextLine();
+				String getCard = userInputStrings.nextLine();
 				UserCard.createCard();
 				UserCard.userBoard= new String [5][5];
 				for (int i=0; i<5; i++)
@@ -87,7 +103,7 @@ public class Bingo
 					}
 				UserCard.display();
 				System.out.println(name + ", press enter to get Jeff's card");
-//				String jeffCard = userInput.nextLine();
+				String jeffCard = userInputStrings.nextLine();
 				ComputerCard.createCard();
 				ComputerCard.jeffBoard= new String [5][5];
 				for (int i=0; i<5; i++)
@@ -100,11 +116,14 @@ public class Bingo
 							}
 					}
 				ComputerCard.display();
+				System.out.println("Press enter for your first calling number!");
+				String firstNum= userInputStrings.nextLine();
 			}
 		public static void getHardBingoCards()
 			{
+				gameChoice = 3;
 				System.out.println(name + ", press enter to get your bingo card");
-//				String getCard = userInput.nextLine();
+				String getCard = userInputStrings.nextLine();
 				UserCard.createCard();
 				UserCard.userBoard= new String [5][5];
 				for (int i=0; i<5; i++)
@@ -118,7 +137,7 @@ public class Bingo
 					}
 				UserCard.display();
 				System.out.println(name + ", press enter to get Jeff's card");
-//				String jeffCard = userInput.nextLine();
+				String jeffCard = userInputStrings.nextLine();
 				ComputerCard.createCard();
 				ComputerCard.jeffBoard= new String [5][5];
 				for (int i=0; i<5; i++)
@@ -131,8 +150,10 @@ public class Bingo
 							}
 					}
 				ComputerCard.display();
+				System.out.println("Press enter for your first calling number!");
+				String firstNum= userInputStrings.nextLine();
 			}
-		public static void playGame()
+		public static void playBingoGame()
 			{
 				int callingNum = 0;
 				boolean playingGame = true;
@@ -150,78 +171,42 @@ public class Bingo
 							{
 								callingNum = ((int)(Math.random()*90)+10);
 							}
-						System.out.println(callingNum);
+						System.out.println("The calling number is " + callingNum);
 						String callingNumber = Integer.toString(callingNum);
 						for (int i=0; i<UserCard.userBoard.length; i++)
 						{
 							for (int j =0; j<UserCard.userBoard[0].length; j++)
 								{
-//									System.out.println(UserCard.userBoard[i][j]);
-//									System.out.println(callingNumber);
 									if (UserCard.userBoard[i][j].equals(callingNumber))
 										{
 											System.out.println("You have " + callingNumber);
-											System.out.println("Your Board");
+											System.out.println("Your Board:");
 											UserCard.userBoard[i][j] = "XX";
 											UserCard.display();
+											UserCard.isBingo();
 										}
 									if (ComputerCard.jeffBoard[i][j].equals(callingNumber))
 										{
 											System.out.println("Jeff has " + callingNumber);
-											System.out.println("Jeff's Board");
+											System.out.println("Jeff's Board:");
 											ComputerCard.jeffBoard[i][j] = "XX";
 											ComputerCard.display();
+											ComputerCard.isBingo();
 										}
 								}
 						}
 						playingGame=false;
 					}
 				UserCard.isBingo();
+				ComputerCard.isBingo();
 				System.out.println("Press enter for the next calling number");
-				String nextNum= userInput.nextLine();
-				playGame();
+				String nextNum= userInputStrings.nextLine();
+				playBingoGame();
+			}
+		public static void blackoutGame()
+			{
+				System.out.println("Do you want to continue and see if you can get a blackout?");
+				String playBlackoutMaybe = userInputStrings.nextLine();
 				
 			}
-		public void isBingo(char[][] array,char g)
-			{
-				boolean bingo = false;
-				int countB=0;
-				for(int i=0;i<array.length;i++)
-					{
-						for(int j=0;j<=i;j++)
-							{
-								if(array[0][j]==g&&array[1][j]==g&&array[2][j]==g&&array[3][j]==g&&array[4][j]==g)
-									{
-										bingo = true;
-										countB++;
-									}
-								else if(array[i][0]==g&&array[i][1]==g&&array[i][2]==g&&array[i][3]==g&&array[i][4]==g)
-									{
-										bingo = true;
-										countB++;
-									}
-								else if(array[0][0]==g&&array[1][1]==g&&array[2][2]==g&&array[3][3]==g&&array[4][4]==g)
-									{
-										bingo =true;
-										countB++;
-									}
-								else if(array[0][4]==g&&array[1][3]==g&&array[2][2]==g&&array[3][1]==g&&array[4][0]==g)
-									{
-										bingo= true;
-										countB++;
-									}
-								else
-									{
-										bingo = false;
-									}
-							}
-					}  
-		}
-		public static void callingNumber()
-			{
-				Random generate = new Random();
-			    String[] randomName = {"B", "I", "N", "G", "O"};
-			    System.out.println(randomName[generate.nextInt(4)] + ((int)(Math.random()*90)+10));
-			}
-		
 	}
--- Bingo Game/src/ComputerCard.java
+++ Bingo Game/src/ComputerCard.java
@@ -30,4 +30,55 @@ public class ComputerCard
 					System.out.println("    |  " + jeffBoard[4][0] +"  |  "+ jeffBoard[4][1] + "  |  " + jeffBoard [4][2] + "  |  " + jeffBoard[4][3] + "  |  " + jeffBoard[4][4] + "  | ");
 					System.out.println("    ------------------------------------");
 				}
+				
+				public static boolean isBingo()
+					{
+						if ((jeffBoard[0][0].equals(jeffBoard[0][1]) && jeffBoard[0][1].equals(jeffBoard[0][2]) && jeffBoard[0][2].equals(jeffBoard[0][3]) && jeffBoard[0][3].equals(jeffBoard[0][4]) && !jeffBoard[0][1].equals("  ")) ||
+							(jeffBoard[1][0].equals(jeffBoard[1][1]) && jeffBoard[1][1].equals(jeffBoard[1][2]) && jeffBoard[1][2].equals(jeffBoard[1][3]) && jeffBoard[1][3].equals(jeffBoard[1][4]) && !jeffBoard[1][1].equals("  ")) ||
+							(jeffBoard[2][0].equals(jeffBoard[2][1]) && jeffBoard[2][1].equals(jeffBoard[2][3]) && jeffBoard[2][3].equals(jeffBoard[2][4]) && !jeffBoard[2][1].equals("  ")) ||
+							(jeffBoard[3][0].equals(jeffBoard[3][1]) && jeffBoard[3][1].equals(jeffBoard[3][2]) && jeffBoard[3][2].equals(jeffBoard[3][3]) && jeffBoard[3][3].equals(jeffBoard[3][4]) && !jeffBoard[3][1].equals("  ")) ||
+							(jeffBoard[4][0].equals(jeffBoard[4][1]) && jeffBoard[4][1].equals(jeffBoard[4][2]) && jeffBoard[4][2].equals(jeffBoard[4][3]) && jeffBoard[4][3].equals(jeffBoard[4][4]) && !jeffBoard[4][1].equals("  ")) ||
+							//across
+							(jeffBoard[0][0].equals(jeffBoard[1][0]) && jeffBoard[1][0].equals(jeffBoard[2][0]) && jeffBoard[2][0].equals(jeffBoard[3][0]) && jeffBoard[3][0].equals(jeffBoard[4][0]) && !jeffBoard[1][0].equals("  ")) ||
+							(jeffBoard[0][1].equals(jeffBoard[1][1]) && jeffBoard[1][1].equals(jeffBoard[2][1]) && jeffBoard[2][1].equals(jeffBoard[3][1]) && jeffBoard[3][1].equals(jeffBoard[4][1]) && !jeffBoard[1][1].equals("  ")) ||
+							(jeffBoard[0][2].equals(jeffBoard[1][2]) && jeffBoard[1][2].equals(jeffBoard[2][2]) && jeffBoard[2][2].equals(jeffBoard[3][2]) && jeffBoard[3][2].equals(jeffBoard[4][2]) && !jeffBoard[1][2].equals("  ")) ||
+							(jeffBoard[0][3].equals(jeffBoard[1][3]) && jeffBoard[1][3].equals(jeffBoard[2][3]) && jeffBoard[2][3].equals(jeffBoard[3][3]) && jeffBoard[3][3].equals(jeffBoard[4][3]) && !jeffBoard[1][3].equals("  ")) ||
+							(jeffBoard[0][4].equals(jeffBoard[1][4]) && jeffBoard[1][4].equals(jeffBoard[2][4]) && jeffBoard[2][4].equals(jeffBoard[3][4]) && jeffBoard[3][4].equals(jeffBoard[4][4]) && !jeffBoard[1][4].equals("  ")) ||
+							//down
+							(jeffBoard[0][0].equals(jeffBoard[1][1]) && jeffBoard[1][1].equals(jeffBoard[3][3]) && jeffBoard[3][3].equals(jeffBoard[4][4]) && !jeffBoard[0][1].equals("  ")) ||
+							(jeffBoard[0][4].equals(jeffBoard[1][3]) && jeffBoard[1][3].equals(jeffBoard[3][1]) && jeffBoard[3][1].equals(jeffBoard[4][0]) && !jeffBoard[0][1].equals("  ")))
+							//diagonal 
+							{
+								System.out.println("Jeff has a Bingo!");
+								System.out.println("Good game! You failed to beat the computer");
+								System.exit(0);
+								return true;
+							}
+						return false;
+					}
+				public static boolean isBlackout()
+					{
+						if ((jeffBoard[0][0].equals(jeffBoard[0][1]) && jeffBoard[0][1].equals(jeffBoard[0][2]) && jeffBoard[0][2].equals(jeffBoard[0][3]) && jeffBoard[0][3].equals(jeffBoard[0][4]) && !jeffBoard[0][1].equals("  ")) &&
+								(jeffBoard[1][0].equals(jeffBoard[1][1]) && jeffBoard[1][1].equals(jeffBoard[1][2]) && jeffBoard[1][2].equals(jeffBoard[1][3]) && jeffBoard[1][3].equals(jeffBoard[1][4]) && !jeffBoard[1][1].equals("  ")) &&
+								(jeffBoard[2][0].equals(jeffBoard[2][1]) && jeffBoard[2][1].equals(jeffBoard[2][3]) && jeffBoard[2][3].equals(jeffBoard[2][4]) && !jeffBoard[2][1].equals("  ")) ||
+								(jeffBoard[3][0].equals(jeffBoard[3][1]) && jeffBoard[3][1].equals(jeffBoard[3][2]) && jeffBoard[3][2].equals(jeffBoard[3][3]) && jeffBoard[3][3].equals(jeffBoard[3][4]) && !jeffBoard[3][1].equals("  ")) &&
+								(jeffBoard[4][0].equals(jeffBoard[4][1]) && jeffBoard[4][1].equals(jeffBoard[4][2]) && jeffBoard[4][2].equals(jeffBoard[4][3]) && jeffBoard[4][3].equals(jeffBoard[4][4]) && !jeffBoard[4][1].equals("  ")) &&
+								//across
+								(jeffBoard[0][0].equals(jeffBoard[1][0]) && jeffBoard[1][0].equals(jeffBoard[2][0]) && jeffBoard[2][0].equals(jeffBoard[3][0]) && jeffBoard[3][0].equals(jeffBoard[4][0]) && !jeffBoard[1][0].equals("  ")) &&
+								(jeffBoard[0][1].equals(jeffBoard[1][1]) && jeffBoard[1][1].equals(jeffBoard[2][1]) && jeffBoard[2][1].equals(jeffBoard[3][1]) && jeffBoard[3][1].equals(jeffBoard[4][1]) && !jeffBoard[1][1].equals("  ")) &&
+								(jeffBoard[0][2].equals(jeffBoard[1][2]) && jeffBoard[1][2].equals(jeffBoard[2][2]) && jeffBoard[2][2].equals(jeffBoard[3][2]) && jeffBoard[3][2].equals(jeffBoard[4][2]) && !jeffBoard[1][2].equals("  ")) &&
+								(jeffBoard[0][3].equals(jeffBoard[1][3]) && jeffBoard[1][3].equals(jeffBoard[2][3]) && jeffBoard[2][3].equals(jeffBoard[3][3]) && jeffBoard[3][3].equals(jeffBoard[4][3]) && !jeffBoard[1][3].equals("  ")) &&
+								(jeffBoard[0][4].equals(jeffBoard[1][4]) && jeffBoard[1][4].equals(jeffBoard[2][4]) && jeffBoard[2][4].equals(jeffBoard[3][4]) && jeffBoard[3][4].equals(jeffBoard[4][4]) && !jeffBoard[1][4].equals("  ")) &&
+								//down
+								(jeffBoard[0][0].equals(jeffBoard[1][1]) && jeffBoard[1][1].equals(jeffBoard[3][3]) && jeffBoard[3][3].equals(jeffBoard[4][4]) && !jeffBoard[0][1].equals("  ")) &&
+								(jeffBoard[0][4].equals(jeffBoard[1][3]) && jeffBoard[1][3].equals(jeffBoard[3][1]) && jeffBoard[3][1].equals(jeffBoard[4][0]) && !jeffBoard[0][1].equals("  ")))
+								//diagonal 
+								{
+									System.out.println("Jeff got a blackout!");
+									System.out.println("Congrats, you failed to beat the computer!");
+									System.exit(0);
+									return true;
+								}
+						return false;
+					}
 	}
